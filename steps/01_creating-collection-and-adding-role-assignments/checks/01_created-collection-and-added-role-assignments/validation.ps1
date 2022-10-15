cd8c433693d56df7a91af3f20de834db8523b977
Nivedha G <109720688+Nivedha-govindan@users.noreply.github.com>
2022-10-15 14:23:55 +0530

    Added validation.ps1 file

---
+++ steps/01_creating-collection-and-adding-role-assignments/checks/01_created-collection-and-added-role-assignments/validation.ps1
@@ -0,0 +1,46 @@
+$WorkSpace = Get-AzResource -ResourceGroupName az-purview-labs -Resourcetype Microsoft.Purview/accounts
+$WorkSpacename = $WorkSpace.Name
+$token = (Get-AzAccessToken -Resource "https://purview.azure.net").Token
+
+$headers = @{ Authorization = "Bearer $token" }
+$uri = "https://"+$WorkSpacename+".purview.azure.com/account/collections/?api-version=2019-11-01-preview"
+
+$res = Invoke-RestMethod -Method Get -ContentType "application/json" -Uri $uri -Headers $headers
+
+
+foreach($i in $res.value)
+{
+    foreach($j in $i)
+    {
+        if($j.friendlyName -like "Contoso")
+        {
+            $collection = $j.friendlyName
+        }
+    }
+}
+
+if($collection){
+    Write-Host "Contoso collection is created"
+}
+else{
+    Write-Host "Contoso collection is not created"
+}
+
+
+foreach($i in $res.value)
+{
+    foreach($j in $i)
+    {
+        if($j.friendlyName -like "Sub Contoso")
+        {
+            $collection = $j.friendlyName
+        }
+    }
+}
+
+if($collection){
+    Write-Host "Sub Contoso collection is created"
+}
+else{
+    Write-Host "Sub Contoso collection is not created"
+}
