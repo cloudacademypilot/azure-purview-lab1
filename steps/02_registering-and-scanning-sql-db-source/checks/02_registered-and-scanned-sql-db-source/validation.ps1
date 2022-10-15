3ca0ada9a3be5a78ad8875d54bdca80021a56d8f
Nivedha G <109720688+Nivedha-govindan@users.noreply.github.com>
2022-10-15 14:27:19 +0530

    Updated validation.ps1

--- steps/02_registering-and-scanning-sql-db-source/checks/02_registered-and-scanned-sql-db-source/validation.ps1
+++ steps/02_registering-and-scanning-sql-db-source/checks/02_registered-and-scanned-sql-db-source/validation.ps1
@@ -17,17 +17,3 @@ else{
 }
 
 
-$DataSource2 = Get-AzPurviewDataSource -Endpoint $TakeEndpoint -Name 'Adls-Gen2'
-if($DataSource2){
-    Write-Host "ADLS Gen2 Account is Registered"
-}
-else{
-    Write-Host "ADLS Gen2 Account is not Registered"
-}
-$SourceAdlsscan = Get-AzPurviewScan -Endpoint $TakeEndpoint -DataSourceName 'Adls-Gen2' -Name 'Scan-adls'
-if($SourceAdlsscan){
-    Write-Host "ADLS Gen2 Scan is succeeded"
-}
-else{
-    Write-Host "ADLS Gen2 Scan is not succeeded"
-}
