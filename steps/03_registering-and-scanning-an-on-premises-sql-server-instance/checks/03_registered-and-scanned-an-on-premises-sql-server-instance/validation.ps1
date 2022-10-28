34d22118d2611132bbb15012b5fb6983ee573f4c
Nivedha G <109720688+Nivedha-govindan@users.noreply.github.com>
2022-10-28 11:57:15 +0530

    Updated validation.ps1

--- steps/03_registering-and-scanning-an-on-premises-sql-server-instance/checks/03_registered-and-scanned-an-on-premises-sql-server-instance/validation.ps1
+++ steps/03_registering-and-scanning-an-on-premises-sql-server-instance/checks/03_registered-and-scanned-an-on-premises-sql-server-instance/validation.ps1
@@ -5,15 +5,25 @@ param(
 $WorkSpace = Get-AzResource -ResourceGroupName $Resourcegroupname -Resourcetype Microsoft.Purview/accounts
 $WorkSpacename = $WorkSpace.Name
 $TakeEndpoint = 'https://'+$WorkSpacename+'.purview.azure.com'
-$DataSource = Get-AzPurviewDataSource -Endpoint $TakeEndpoint -Name 'SqlServer-onprem'
-if($DataSource){
-    Write-Host "SQL Server is Registered"
+
+$DataSource = Get-AzPurviewDataSource -Endpoint $TakeEndpoint
+foreach($Source in $DataSource)
+{
+    if($Source.kind -like "SqlServerDatabase")
+    {
+        $Onpremsource = $Source.name
+    }
+}
+$DataSourceValidate = Get-AzPurviewDataSource -Endpoint $TakeEndpoint -Name $Onpremsource
+if($DataSourceValidate){
+    Write-Host "On-Prem SQL Server is Registered"
 }
 else{
-    Write-Host "SQL Server is not Registered"
+    Write-Host "On-Prem SQL Server is not Registered"
 }
-$SourceSqlServerscan = Get-AzPurviewScan -Endpoint $TakeEndpoint -DataSourceName 'SqlServer-onprem' -Name 'Scan-instance'
-if($SourceSqlServerscan){
+
+$Sourcescan = Get-AzPurviewScan -Endpoint $TakeEndpoint -DataSourceName $Onpremsource
+if($Sourcescan){
     Write-Host "SQL Server Scan is succeeded"
 }
 else{
